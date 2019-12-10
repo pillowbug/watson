3156394e6215dbfed019875c286620ee68781ed7
Gerard Cabarse <gerard.cabarse@gmail.com>
2019-12-10 15:30:25 -0800

    fixed routing - added pages for show index + new

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,18 +1,22 @@
 class ShowsController < ApplicationController
 
   def index
+    @shows = Show.all
   end
 
   def show
+    @show = Show.find(params[:id])
   end
 
   def new
-  end
-
-  def edit
+    @show = Show.new
   end
 
   def create
+    @show = Show.new(show_params)
+  end
+
+  def edit
   end
 
   def destroy

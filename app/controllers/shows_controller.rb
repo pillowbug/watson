7e6c1266715ff4e4ce1949a4b8a411202dc042db
Gerard Cabarse <gerard.cabarse@gmail.com>
2019-12-10 12:01:08 -0800

    configuring show index

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,2 +1,21 @@
 class ShowsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
 end
